9552cdb27aaa04c42d6aa00eb1e6568d68e2e8c0
Dan Murphy <dmurphy79@gmail.com>
2020-09-01 13:14:20 -0500

    adds questions query

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export const App: React.FC = () => (
-  <div
-    style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-  >
-    <h1>Lucid</h1>
-    <h2>Welcome to UI Team code assessment!</h2>
-  </div>
-);
+const QUESTIONS = gql`
+  query GetQuestions {
+    questions {
+      question
+      type
+      correct_answer
+      incorrect_answers
+    }
+  }
+`;
+
+export const App: React.FC = () => {
+  const { loading, error, data } = useQuery(QUESTIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Lucid</h1>
+      <h2>Welcome to UI Team code assessment!</h2>
+    </div>
+  );
+};
