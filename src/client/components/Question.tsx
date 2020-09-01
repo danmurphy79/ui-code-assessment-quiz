47a09e8683155a3e8c748a3cb4f2160498de3cb3
Dan Murphy <dmurphy79@gmail.com>
2020-09-01 09:01:03 -0500

    Lots of changes that aren't quite getting me where I need to be but are getting me closer

--- src/client/components/Question.tsx
+++ src/client/components/Question.tsx
@@ -1,36 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, Dispatch, SetStateAction, useEffect } from "react";
 
 // TODO: Uncomment when this is being passed down
 export interface QuestionProps {
   questions: Array<any>;
+  questionCount: number;
+  setQuestionCount: Dispatch<SetStateAction<number>>;
+  correctAnswerCount: number;
+  setCorrectAnswerCount: Dispatch<SetStateAction<number>>;
+  wrongAnswerCount: number;
+  setWrongAnswerCount: Dispatch<SetStateAction<number>>;
 }
 
-const Question: React.FC<QuestionProps> = ({ questions }) => {
-  const [randomQuestion, setRandomQuestion] = useState<null | {
+interface AnswerCountProps {
+  questions: number;
+  wrongAnswers: number;
+  correctAnswers: number;
+}
+
+const Question: React.FC<QuestionProps> = ({
+  questions,
+  questionCount,
+  setQuestionCount,
+  correctAnswerCount,
+  setCorrectAnswerCount,
+  wrongAnswerCount,
+  setWrongAnswerCount,
+}) => {
+  //TODO: handle the randomness in the GraphQL query
+  const [currentQuestion, setCurrentQuestion] = useState<null | {
     correct_answer: string;
     incorrect_answers: [string];
-    question: String;
-    type: String;
-  }>(questions[Math.floor(Math.random() * questions.length)]);
+    question: string;
+    type: string;
+  }>(questions[0]);
+
+  const [chosenAnswer, setAnswer] = useState<any>();
+  useEffect(() => {
+    setCurrentQuestion(questions[1]);
+    setAnswer("");
+  }, [questionCount]);
 
   const allAnswers =
-    randomQuestion!.type !== "text"
-      ? randomQuestion!.incorrect_answers.concat(randomQuestion!.correct_answer)
+    currentQuestion!.type !== "text"
+      ? currentQuestion!.incorrect_answers.concat(
+          currentQuestion!.correct_answer
+        )
       : [];
 
   const handleClick = () => {
-    setRandomQuestion(questions[Math.floor(Math.random() * questions.length)]);
+    console.log("click");
+    console.log(questionCount);
+    // if (chosenAnswer === currentQuestion!.correct_answer) {
+    //   setCorrectAnswerCount(correctAnswerCount++);
+    // } else {
+    //   setWrongAnswerCount(wrongAnswerCount++);
+    // }
+    setQuestionCount(questionCount++);
+    console.log(questionCount);
   };
+
   return (
     <>
-      <div>{randomQuestion!.question}</div>
+      <div>{currentQuestion!.question}</div>
 
-      {randomQuestion!.type !== "text" ? (
+      {currentQuestion!.type !== "text" ? (
         <form>
           {allAnswers.map((answer, i) => {
             return (
               <div key={i}>
-                <input type="radio" name="answer" id={answer}></input>
+                <input
+                  onChange={(e) => setAnswer(e.target.value)}
+                  type="radio"
+                  name="answer"
+                  id={answer}
+                  value={chosenAnswer}
+                ></input>
                 <label htmlFor={answer}> {answer}</label>
               </div>
             );
