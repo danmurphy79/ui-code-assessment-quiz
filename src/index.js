dc538be1fadbb846bb457db41c0273a2a66ad7f8
Dan Murphy <dmurphy79@gmail.com>
2020-09-01 12:55:24 -0500

    adds ApolloProvider Wrapper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './client/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./client/App";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+//TODO: remove this sample code
+client
+  .query({
+    query: gql`
+      {
+        questions {
+          question
+          type
+          correct_answer
+          incorrect_answers
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
