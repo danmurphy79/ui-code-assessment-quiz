fca179e9788d69a341dc3fe38b92ea33517421f4
Dan Murphy <dmurphy79@gmail.com>
2020-09-02 18:02:18 -0500

    last-minute discovery of an apostrophe bug

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,10 @@
-import { unescape, replace } from "lodash";
+import { unescape } from "lodash";
 
 export function formatString(string: string): string {
   // edge case - &#039; isn't converted by _.unescape()
   if (string.includes("&#039;")) {
-    const apostropheString = replace(string, "&#039;", "&#39;");
+    const apostropheString = string.replace(/&#039;/g, "&#39;");
+
     return unescape(apostropheString);
   }
 
