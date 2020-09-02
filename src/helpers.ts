9530e04db05cd02c0894ce874df9dd94bde29379
Dan Murphy <dmurphy79@gmail.com>
2020-09-02 09:37:55 -0500

    adds apostrophe edge case

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,12 @@
-import { unescape } from "lodash";
+import { unescape, replace } from "lodash";
+
 export function formatString(string: string) {
+  // edge case - &#039; isn't converted by _.unescape()
+  if (string.includes("&#039;")) {
+    const apostropheString = replace(string, "&#039;", "&#39;");
+    return unescape(apostropheString);
+  }
+
   const formattedString = unescape(string);
   return formattedString;
 }
